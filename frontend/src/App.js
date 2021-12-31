d134267eeb8f40dc7e39d1169fd7450cc3b9b1d7
Alex Crooks <alexcrooks2@gmail.com>
2021-12-30 17:14:48 -0800

    First deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [getMessage, setGetMessage] = useState({})
 
   useEffect(()=>{
-    axios.get('http://localhost:5000/flask/hello').then(response => {
+    axios.get('https://betterbinge.herokuapp.com/flask/hello').then(response => {
       console.log("SUCCESS", response)
       setGetMessage(response)
     }).catch(error => {
